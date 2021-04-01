d0faf63257bc6bae1027b615bea89c9664c3d791
Tamaghno <tamaghno.chaudhuri@gmail.com>
2021-04-01 22:05:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
+
+mongoose.connect(process.env.MONGO_URI,
+{ useNewUrlParser: true },
+{ useUnifiedTopology: true })
+.then(()=> console.log('DB connected'))
+.catch(err => console.error(err))
+
+
+
+// middleware
+app.use('/posts', ()=>{
+    console.log('this is a middleware running');
+});
+
+
+// routes
+app.get('/',(req, res) => {
+    res.send('home e achi');
+});
+
+// app.get('/posts',(req, res) => {
+//     res.send('posts e achi');
+// });
+
+
+
+// listening to the server
+app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+const Post = require('../models/Post')
+
+
+// get back all posts
+router.get('/', async (req, res) => {
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }
+    catch(err){res.json({message: err})}
+});
+
+//retrieve a specific post through id in url
+router.get('/:postId', async (req, res) => {
+    // console.log(req.params.postId);
+    try{
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }
+    catch(err){res.json({message : err})}
+})
+
+
+
+
+//submits a post
+router.post('/', async(req, res) => {
+    // console.log(req.body);
+    const post = new Post({
+        title:req.body.title,
+        description:req.body.description
+    })
+
+    try{
+        const savedPost = await post.save();
+        res.json(savedPost);
+    }
+    catch(err){res.json({message :err})}
+    
+})
+
+//delete a speific post
+router.delete('/:postId', async (req, res) => {
+    // console.log(req.params.postId);
+    try{
+        const removedPost = await Post.remove({_id: req.params.postId});
+        res.json(removedPost);
+    }
+    catch(err){res.json({message : err})}
+})
+
+
+
+
+// update a specific postId
+router.patch('/:postId', async (req, res) => {
+    // console.log(req.params.postId);
+    try{
+        const updatedPost = await Post.updateOne(
+            {_id: req.params.postId}, 
+            {$set: {title: req.body.title}});
+        res.json(updatedPost);
+    }
+    catch(err){res.json({message : err})}
+})
+
+
+// router.get('/specific',(req, res) => {
+//     res.send('specific post e achi');
+// });
+
+
+
+module.exports = router;
