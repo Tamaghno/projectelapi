d04ed37614837addcbc56dfe77ed7f46161befcb
Tamaghno <tamaghno.chaudhuri@gmail.com>
2021-04-01 22:43:40 +0530

    dhur

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.use(bodyParser.json());
 const postsRoute = require('./routes/posts');
 app.use('/posts', postsRoute);
 
-mongoose.connect(
-'mongodb+srv://tamaghno:tamaghno@cluster0.aiha3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGO_URI,
 { useNewUrlParser: true },
 { useUnifiedTopology: true })
 .then(()=> console.log('DB connected'))
